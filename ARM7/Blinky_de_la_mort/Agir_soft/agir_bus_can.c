d9ecf8a6f5971f902221c01333ba7d89de586f0c
Lounes <achab.lounes@gmail.com>
2015-03-10 09:23:44 +0100

    Creation du fichier header pour le bus can , et programmation de fonction de receptions et emissions

--- ARM7/Blinky_de_la_mort/Agir_soft/agir_bus_can.c
+++ ARM7/Blinky_de_la_mort/Agir_soft/agir_bus_can.c
@@ -16,15 +16,7 @@
 
 #include "lpc23xx.h"
 #include <stdio.h> 
-
-
-#define SIZE_MSG 1
-#define ID_TEST_01 11
-#define ID_TEST_02 22
-
-char ETAT = 0;
-unsigned long RECEPTION	;
-
+#include "agir_bus_can.h"
 
 /**********************************************************/
 /********************* Initialisation *********************/
@@ -39,4 +31,48 @@ void init_CAN(void)
 	CAN1IER	 = 0x01; 	 // autorise les interuptions RX
 	CAN_AFMR  =	0x02; 	 // desactive le filtre CAN1
 	CAN1MOD	 =	0x00;  	 // mode 0 can 
+}
+
+
+
+/**********************************************************/
+/************** Fonctions d'envoie de donnees *************/
+/**********************************************************/
+
+void can_emission(unsigned long valeur ,unsigned char id_message)
+{
+	if(CAN1SR & 0x400)
+	{
+		CAN1TFI2 = 0x10000	;  // Taille data 1 octet
+		CAN1TID2 = id_message; // id message = 40 
+		CAN1TDA2 = valeur;     // Ecriture data dans buffer TX2
+		CAN1CMR  = 0x41;       // Envoie du message 	
+	}
+}
+
+/**********************************************************/
+/************* Fonctions Reception de donnees *************/
+/**********************************************************/
+
+void isr_can_reception(void)__irq
+{
+	unsigned long id_message = (CAN1RID & 0x3FF);
+
+	switch(id_message)
+	{
+		case ID_TEST_01	:
+			RECEPTION = CAN1RDA;
+		 	break;
+
+		case ID_TEST_02	:
+			RECEPTION = CAN1RDA;
+			break;
+
+		default:
+			RECEPTION = 0;
+			break;
+	}
+		
+	CAN1CMR 	= 0x4; // vide buffer RX
+	VICVectAddr = 0x0;
 }
---
+++ ARM7/Blinky_de_la_mort/Agir_soft/agir_bus_can.h
@@ -0,0 +1,24 @@
+/**********************************************************/
+/*===================== PROJET AGIR ======================*/
+/*--------------------------------------------------------*/
+/*-- Projet AGIR [EI-SE4] 2014/2015 Polytech Paris'UPMC --*/
+/*-- [GITHUB]: https://github.com/Hapinness/Projet_AGIR --*/
+/*-- [MAIL]  :	polytech.projetagir@gmail.com            --*/
+/*-- [FB]    :	Projet.Agir@groups.facebook.com          --*/
+/*========================================================*/
+/*----------------- [PROGRAMMEURS MAIL] ------------------*/
+/*-- {Achab Lounes}      : achab.lounes@gmail.com       --*/
+/*-- {Afonso Jordan}     : afonso.jordan@hotmail.fr     --*/
+/*-- {Kalghdoost Afshin} : afshin_90@hotmail.fr         --*/
+/*-- {Younsi Thin-hinane}: thin-hinane@hotmail.fr       --*/
+/*========================================================*/
+/**********************************************************/
+
+#define SIZE_MSG 1
+#define ID_MODULE_01 01
+#define ID_MODULE_02 02
+#define ID_MODULE_03 03
+#define ID_MODULE_04 04
+
+char ETAT = 0;
+unsigned long RECEPTION	;
